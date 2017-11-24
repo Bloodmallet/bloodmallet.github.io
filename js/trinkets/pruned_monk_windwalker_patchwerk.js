1ccf7995549ed9144385fda842a79a484c6c0f37
Bloodmallet <kruse.peter.1990@gmail.com>
2017-11-24 03:27:10 +0100

    Add forgotten Monks for Antorus

---
+++ js/trinkets/pruned_monk_windwalker_patchwerk.js
@@ -0,0 +1,345 @@
+Highcharts.chart('pruned_monk_windwalker_patchwerk', 
+{
+    chart: {
+        type: "bar"
+    },
+    legend: {
+        align: "right",
+        backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+        borderColor: "#CCC",
+        borderWidth: 1,
+        floating: false,
+        reversed: true,
+        shadow: false,
+        verticalAlign: "bottom",
+        x: 0,
+        y: 0
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                enabled: false
+            },
+            point: {
+                events: {
+                    click: function (event) {                var chart = this.series.yAxis;                chart.removePlotLine('helperLine');                chart.addPlotLine({                    value: this.stackY,                    color: '#000',                    width: 2,                    id: 'helperLine',                    zIndex: 5,                    label: {                      text: this.series.name + ' ' + this.category + ': ' + this.stackY,                      align: 'left',                      verticalAlign: 'bottom',                      rotation: 0,                      y: -5                    }                });              }
+                }
+            },
+            stacking: "normal"
+        },
+        series: {
+            borderColor: "#151515",
+            events: {
+                legendItemClick: function() { return false; }
+            }
+        }
+    },
+    series: [
+        {
+            color: "#ffeb3b",
+            data: [
+                249736,
+                243868,
+                188143,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "1000"
+        },
+        {
+            color: "#e600e3",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "980"
+        },
+        {
+            color: "#00e676",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "970"
+        },
+        {
+            color: "#db843d",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                153776,
+                142061,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "960"
+        },
+        {
+            color: "#4198af",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "950"
+        },
+        {
+            color: "#71588f",
+            data: [
+                0,
+                0,
+                0,
+                154899,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "940"
+        },
+        {
+            color: "#89a54e",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                141804,
+                0,
+                128777,
+                0,
+                126057,
+                122458,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "930"
+        },
+        {
+            color: "#aa4643",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "920"
+        },
+        {
+            color: "#4572a7",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                136252,
+                0,
+                128744,
+                0,
+                0,
+                121164,
+                120046,
+                119690,
+                114885,
+                112897,
+                111933,
+                111028,
+                110736
+            ],
+            name: "910"
+        }
+    ],
+    subtitle: {
+        text: "UTC 2017-11-24 01:59 SimC build: <a href=\"https://github.com/simulationcraft/simc/commit/83184853fbe2695894ec9cc2b135d309f2eb2f09\" target=\"blank\">8318485</a>"
+    },
+    title: {
+        text: "Monk - Windwalker - Patchwerk"
+    },
+    tooltip: {
+        backgroundColor: "#eee",
+        borderColor: "#bbb",
+        formatter: function() {        var s = '<b>'+ this.x +'</b>';        var cumulative_amount = 0;        for (var i = this.points.length - 1 ; i >= 0 ; i--) {            cumulative_amount += this.points[i].y;            if (this.points[i].y !== 0){                s += '<br/><span style=\"color: ' + this.points[i].series.color + '; font-weight: bold;\">' + this.points[i].series.name +'</span>: ' + cumulative_amount;            }        }        return s;      },
+        headerFormat: "<b>{point.x}</b>",
+        shared: true,
+        style: {
+            color: "black"
+        }
+    },
+    xAxis: {
+        categories: [
+            "Kil'jaeden's Burning Wish",
+            "Golganneth's Vitality",
+            "Aman'Thul's Vision",
+            "Specter of Betrayal",
+            "Shadow-Singed Fang",
+            "Seeping Scourgewing",
+            "Engine of Eradication",
+            "Eye of Command",
+            "Astral Alchemist Stone",
+            "Unstable Arcanocrystal",
+            "Cradle of Anguish",
+            "Vial of Ceaseless Toxins",
+            "Stat Stick (Versatility)",
+            "Stat Stick (Crit)",
+            "Stat Stick (Mastery)",
+            "Memento of Angerboda",
+            "Entwined Elemental Foci",
+            "PVP Badge of Conquest",
+            "Convergence of Fates",
+            "Bloodthirsty Instinct"
+        ]
+    },
+    yAxis: {
+        labels: {
+            enabled: false
+        },
+        min: 0,
+        plotLines: [
+            {
+                color: "#0973DA",
+                label: {
+                    align: "left",
+                    rotation: 0,
+                    style: {
+                        color: "#0973DA"
+                    },
+                    text: "mean: 141947",
+                    verticalAlign: "bottom",
+                    x: 10,
+                    y: -23
+                },
+                value: 141947.7,
+                width: 2,
+                zIndex: 2
+            }
+        ],
+        stackLabels: {
+            enabled: true
+        },
+        title: {
+            text: "\u0394 Damage per second"
+        }
+    }
+});
---
+++ js/trinkets/pruned_titanforged_monk_windwalker_beastlord.js
@@ -0,0 +1,345 @@
+Highcharts.chart('pruned_titanforged_monk_windwalker_beastlord', 
+{
+    chart: {
+        type: "bar"
+    },
+    legend: {
+        align: "right",
+        backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+        borderColor: "#CCC",
+        borderWidth: 1,
+        floating: false,
+        reversed: true,
+        shadow: false,
+        verticalAlign: "bottom",
+        x: 0,
+        y: 0
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                enabled: false
+            },
+            point: {
+                events: {
+                    click: function (event) {                var chart = this.series.yAxis;                chart.removePlotLine('helperLine');                chart.addPlotLine({                    value: this.stackY,                    color: '#000',                    width: 2,                    id: 'helperLine',                    zIndex: 5,                    label: {                      text: this.series.name + ' ' + this.category + ': ' + this.stackY,                      align: 'left',                      verticalAlign: 'bottom',                      rotation: 0,                      y: -5                    }                });              }
+                }
+            },
+            stacking: "normal"
+        },
+        series: {
+            borderColor: "#151515",
+            events: {
+                legendItemClick: function() { return false; }
+            }
+        }
+    },
+    series: [
+        {
+            color: "#ffeb3b",
+            data: [
+                473377,
+                352210,
+                331702,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "1000"
+        },
+        {
+            color: "#e600e3",
+            data: [
+                0,
+                0,
+                0,
+                40005,
+                24301,
+                0,
+                0,
+                0,
+                0,
+                12814,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "980"
+        },
+        {
+            color: "#00e676",
+            data: [
+                0,
+                0,
+                0,
+                12473,
+                19229,
+                0,
+                0,
+                0,
+                0,
+                29929,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "970"
+        },
+        {
+            color: "#db843d",
+            data: [
+                0,
+                0,
+                0,
+                261477,
+                247339,
+                0,
+                0,
+                24594,
+                0,
+                218984,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "960"
+        },
+        {
+            color: "#4198af",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                0,
+                21842,
+                22755,
+                12264,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "950"
+        },
+        {
+            color: "#71588f",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                0,
+                21091,
+                19893,
+                225211,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            name: "940"
+        },
+        {
+            color: "#89a54e",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                0,
+                229631,
+                227253,
+                0,
+                14285,
+                0,
+                15524,
+                12482,
+                14565,
+                11584,
+                17711,
+                21934,
+                12349,
+                13956,
+                10047,
+                211431
+            ],
+            name: "930"
+        },
+        {
+            color: "#aa4643",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                21441,
+                0,
+                13919,
+                12687,
+                9431,
+                26378,
+                14613,
+                4463,
+                13884,
+                13681,
+                13527,
+                0
+            ],
+            name: "920"
+        },
+        {
+            color: "#4572a7",
+            data: [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                226178,
+                0,
+                219686,
+                221903,
+                222343,
+                194708,
+                198973,
+                204534,
+                203507,
+                196690,
+                194629,
+                0
+            ],
+            name: "910"
+        }
+    ],
+    subtitle: {
+        text: "UTC 2017-11-24 01:59 SimC build: <a href=\"https://github.com/simulationcraft/simc/commit/83184853fbe2695894ec9cc2b135d309f2eb2f09\" target=\"blank\">8318485</a>"
+    },
+    title: {
+        text: "Monk - Windwalker - Beastlord"
+    },
+    tooltip: {
+        backgroundColor: "#eee",
+        borderColor: "#bbb",
+        formatter: function() {        var s = '<b>'+ this.x +'</b>';        var cumulative_amount = 0;        for (var i = this.points.length - 1 ; i >= 0 ; i--) {            cumulative_amount += this.points[i].y;            if (this.points[i].y !== 0){                s += '<br/><span style=\"color: ' + this.points[i].series.color + '; font-weight: bold;\">' + this.points[i].series.name +'</span>: ' + cumulative_amount;            }        }        return s;      },
+        headerFormat: "<b>{point.x}</b>",
+        shared: true,
+        style: {
+            color: "black"
+        }
+    },
+    xAxis: {
+        categories: [
+            "Kil'jaeden's Burning Wish",
+            "Golganneth's Vitality",
+            "Aman'Thul's Vision",
+            "Gorshalach's Legacy",
+            "Shadow-Singed Fang",
+            "Engine of Eradication",
+            "Cradle of Anguish",
+            "Specter of Betrayal",
+            "Bloodthirsty Instinct",
+            "Forgefiend's Fabricator",
+            "Eye of Command",
+            "Unstable Arcanocrystal",
+            "Convergence of Fates",
+            "Thrice-Accursed Compass",
+            "Memento of Angerboda",
+            "Entwined Elemental Foci",
+            "Chaos Talisman",
+            "Chrono Shard",
+            "Stat Stick (Mastery)",
+            "Astral Alchemist Stone"
+        ]
+    },
+    yAxis: {
+        labels: {
+            enabled: false
+        },
+        min: 0,
+        plotLines: [
+            {
+                color: "#0973DA",
+                label: {
+                    align: "left",
+                    rotation: 0,
+                    style: {
+                        color: "#0973DA"
+                    },
+                    text: "mean: 270570",
+                    verticalAlign: "bottom",
+                    x: 10,
+                    y: -23
+                },
+                value: 270570.85,
+                width: 2,
+                zIndex: 2
+            }
+        ],
+        stackLabels: {
+            enabled: true
+        },
+        title: {
+            text: "\u0394 Damage per second"
+        }
+    }
+});
