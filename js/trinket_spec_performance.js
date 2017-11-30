b383f70f171a99cf56d3770c9bc16d52651e1f8f
Bloodmallet <kruse.peter.1990@gmail.com>
2017-11-30 13:33:07 +0100

    deactivate invalid itemlevels, load chart directly after loading data
    https://github.com/Bloodmallet/bloodmallet.github.io/issues/31
    https://github.com/Bloodmallet/bloodmallet.github.io/issues/30

--- js/trinket_spec_performance.js
+++ js/trinket_spec_performance.js
@@ -5,7 +5,7 @@ var loaded_expectation = 0;
 
 var simulated_data = {};
 var simulated_ilevels = [];
-var last_simulation = "PLACEHOLDER Date - SimC Build";
+var last_simulation = "PLACEHOLDER Date - SimC Build (If you can read this, the JSON import might've not worked or is slow.)";
 
 var wow_classes_specs = [
   [
@@ -212,6 +212,7 @@ function wait_for_load() {
  * Populate select forms with data. Create base chart.
  */
 function fill_menu() {
+  // create an array of all trinket names except for "baseline"
   var trinket_list = [];
   for (spec in simulated_data["patchwerk"]) {
     //console.log(spec);
@@ -222,8 +223,11 @@ function fill_menu() {
       }
     }
   }
+  // sort trinket name array
   trinket_list.sort();
   //console.log(trinket_list);
+
+  // fill trinket options with trinket array
   var trinket_choice = document.getElementById("trinket_choice");
   for (trinket in trinket_list) {
     var option = document.createElement("option");
@@ -232,11 +236,12 @@ function fill_menu() {
     trinket_choice.appendChild(option);
   }
 
-  // add select event for both selects
+  // add change event to the trinket options
   trinket_choice.addEventListener("change", function() {
     reload_chart();
   });
 
+  // fill fight_style options
   var fight_style_choice = document.getElementById("fight_style_choice");
   for (fight_style in fight_styles) {
     var option = document.createElement("option");
@@ -245,26 +250,32 @@ function fill_menu() {
     fight_style_choice.appendChild(option);
   }
 
+  // add change event to the fight_style options
   fight_style_choice.addEventListener("change", function() {
     reload_chart();
   });
 
+  // add all simulated itemlevels
   var itemlevel_choice = document.getElementById("itemlevel_choice");
-  for (itemelvel in simulated_ilevels) {
+  for (itemlevel in simulated_ilevels) {
     var option = document.createElement("option");
-    option.innerHTML = simulated_ilevels[itemelvel];
-    option.value = simulated_ilevels[itemelvel];
+    option.innerHTML = simulated_ilevels[itemlevel];
+    option.value = simulated_ilevels[itemlevel];
+    //console.log(option.value);
     itemlevel_choice.appendChild(option);
   }
 
+  // add change event to itemlevels
   itemlevel_choice.addEventListener("change", function() {
     reload_chart();
   });
 
+  // make all three elements visible
   trinket_choice.style.display = "block";
   fight_style_choice.style.display = "block";
   itemlevel_choice.style.display = "block";
 
+  // create chart with deault values
   chart = Highcharts.chart('performance_root',
   {
       chart: {
@@ -330,6 +341,8 @@ function fill_menu() {
       }
   });
 
+  // trigger update function for chart
+  reload_chart();
 }
 
 
@@ -337,43 +350,72 @@ function fill_menu() {
  * Update all data of the chart according to current selection.
  */
 function reload_chart() {
+  // if the chart isn't visible, make it visible
   var chart_div = document.getElementById("performance_root");
   if (chart_div.style.display == "none") {
     chart_div.style.display = "block";
   }
 
+  // get selected values for trinket, fight_style and itemlevel
   var trinket_field = document.getElementById("trinket_choice");
   var fight_style_field = document.getElementById("fight_style_choice");
   var itemlevel_field = document.getElementById("itemlevel_choice");
   var selected_trinket = trinket_field.options[trinket_field.selectedIndex].value;
   var selected_fight_style = fight_style_field.options[fight_style_field.selectedIndex].value;
+
+
+  // prune itemlevels
+  var trinket_found = false;
+  var need_new_start_itemlevel = false;
+  for (spec in simulated_data[selected_fight_style]) {
+    if (!trinket_found && selected_trinket in simulated_data[selected_fight_style][spec]) {
+      // deactivate all itemlevels that have a dps value of zero
+      for (var i = 0; i < itemlevel_field.children.length; i++) {
+        if (simulated_data[selected_fight_style][spec][selected_trinket][itemlevel_field.children[i].getAttribute("value")] == 0) {
+          itemlevel_field.children[i].disabled = true;
+          // special case if the previously selected itemlevel was deactivated
+          if (i == itemlevel_field.selectedIndex) {
+            //console.log("Selected itemlevel was disabled.");
+            need_new_start_itemlevel = true;
+          }
+        } else {
+          itemlevel_field.children[i].disabled = false;
+        }
+      }
+
+      // change which itemlevel is selected to the first nondisabled one if the previous selected was disabled
+      if (need_new_start_itemlevel) {
+        for (var i = 0; i < itemlevel_field.children.length && need_new_start_itemlevel; i++) {
+          if (!itemlevel_field.children[i].disabled) {
+            itemlevel_field.children[i].selected = true;
+            need_new_start_itemlevel = false;
+          }
+        }
+      }
+
+      // variable to scan trinket only in the first found spec that does have the trinket
+      trinket_found = true;
+    }
+  }
+
+
   var selected_itemlevel = itemlevel_field.options[itemlevel_field.selectedIndex].value;
-  //console.log(selected_trinket);
-  //console.log(selected_fight_style);
 
   // set title to the currently selected "trinket - Fight_style"
   chart.setTitle({ text: selected_itemlevel + " " + selected_trinket + " - " + selected_fight_style[0].toUpperCase() + selected_fight_style.substring(1, selected_fight_style.length) });
 
   // collect necessary trinket data
-  // { specname: normalized damage, }
   var valid_specs = [];
-  var trinket_data = {};
-
+  var trinket_data = {};  // { specname: normalized damage, }
   for (spec in simulated_data[selected_fight_style]) {
-    //console.log(spec);
     if (selected_trinket in simulated_data[selected_fight_style][spec] && selected_itemlevel in simulated_data[selected_fight_style][spec][selected_trinket]) {
       valid_specs.push(spec);
-      //console.log(spec);
-      //console.log(simulated_data[selected_fight_style][spec][selected_trinket][selected_itemlevel]);
-      //console.log(simulated_ilevels[simulated_ilevels.length - 1]);
-      //console.log(simulated_data[selected_fight_style][spec]["baseline"][simulated_ilevels[simulated_ilevels.length - 1]]);
       trinket_data[spec] = parseInt(simulated_data[selected_fight_style][spec][selected_trinket][selected_itemlevel]) - parseInt(simulated_data[selected_fight_style][spec]["baseline"][simulated_ilevels[simulated_ilevels.length - 1]]);
     }
   }
   valid_specs.sort();
-  //console.log(trinket_data);
-  //console.log(valid_specs);
 
+  // update categories (spec names) without redrawing the chart
   chart.xAxis[0].setCategories(valid_specs, false);
 
   // delete all previous series
@@ -388,12 +430,9 @@ function reload_chart() {
 
     // save colour and name of the wow_class
     for (wow_class in wow_classes_specs) {
-      //console.log(spec);
-      //console.log(wow_classes_specs[wow_class]);
       if (spec.startsWith(wow_classes_specs[wow_class][0])) {
         spec_colour = wow_classes_specs[wow_class][2];
         class_name = spec;
-        //console.log(spec_colour);
       }
     }
 
@@ -414,7 +453,7 @@ function reload_chart() {
       data: spec_data,
     };
 
-    // add generated series data to chart
+    // add generated series data to chart without redrawing the chart
     chart.addSeries(series, false);
   }
 
